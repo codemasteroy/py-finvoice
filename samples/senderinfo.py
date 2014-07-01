24e32a367691fb32c5b9561a1b50c93bef17417d
S H Mohanjith <moha@mohanjith.net>
2014-07-01 14:20:33 +0300

    Better Samples

--- samples/senderinfo.py
+++ samples/senderinfo.py
@@ -1,12 +1,117 @@
 #!/usr/bin/env python
 # -*- coding: iso8859-15 -*-
 
+
+## Start of parameters ##
+
+_sellerOrganisationName = {
+	'FI': 'Pullis Musiken Oy',
+	'SV': 'Pullis Musiken Ab',
+	'EN': 'Pullis Musiken Ltd',
+}
+_sellerAddress = 'Puukatu 2 F'
+_sellerTown = 'HELSINKI'
+_sellerPostCode = '00112'
+_sellerCountryCode = 'FI'
+_sellerCountryName = 'Suomi'
+_sellerAccounts = [
+	{ 'IBAN': 'FI2757800750155448', 'BIC': 'OKOYFIHH' },
+	{ 'IBAN': 'FI2721221222212227', 'BIC': 'NDEAFIHH' },
+	{ 'IBAN': 'FI2781232323312334', 'BIC': 'PSPBFIHH' },
+]
+
+_sellerWebAddressNameText = _sellerOrganisationName['FI']
+_sellerWebAddress = 'https://www.pullinmusiikki.fi/'
+_sellerInvoiceAddress = '00371999207'
+_sellerInvoiceIntermediatorAddress = 'OKOYFIHH'
+_sellerYTunnus = '0199920-7'
+_sellerIndustryCode = '62020'
+
+_sellerInvoiceTypeDetails = {
+	'FI': { 
+		'text': 'Kirjanpito palvelu',
+		'validation':
+		[
+			{
+				'type': '02',
+				'min': None,
+				'hyphens': None,
+				'spaces': None,
+				'max': None,
+				'text': 'Viitenumero'
+			},
+			{
+				'type': '09',
+				'min': 10,
+				'hyphens': True,
+				'spaces': None,
+				'max': 10,
+				'text': 'Asiakasnumero'
+			},
+		],
+	},
+	'SV': { 
+		'text': 'Bokföring tjänster',
+		'validation':
+		[
+			{
+				'type': '02',
+				'min': None,
+				'hyphens': None,
+				'spaces': None,
+				'max': None,
+				'text': 'Referensnummer'
+			},
+			{
+				'type': '09',
+				'min': 10,
+				'hyphens': True,
+				'spaces': None,
+				'max': 10,
+				'text': 'Kundnummer'
+			},
+		],
+	},
+	'EN': {
+		'text': 'Bookkeeping service',
+		'validation':
+		[
+			{
+				'type': '02',
+				'min': None,
+				'hyphens': None,
+				'spaces': None,
+				'max': None,
+				'text': 'Reference number'
+			},
+			{
+				'type': '09',
+				'min': 10,
+				'hyphens': True,
+				'spaces': None,
+				'max': 10,
+				'text': 'Customer number'
+			},
+		],
+	},
+}
+
+_paymentInstructionId = 'Bookkeeping service'
+
+_proposedDueDate = 'NO'
+_proposedPaymentPeriod = 'YES'
+
+_messageId = '001'
+_messageActionCode = 'ADD'
+
+## End of parameters ##
+
 import sys
 import datetime, time
 
-reload(sys)
+reload( sys )
 
-sys.setdefaultencoding("iso8859-15")
+sys.setdefaultencoding( 'iso8859-15' )
 sys.path.append( './' )
 
 from finvoice.sender.senderinfo import ExternalEncoding
@@ -36,132 +141,161 @@ nowDate = date( "CCYYMMDD", datetime.datetime.now( reference.LocalTimezone() ).d
 
 # Seller Postal Address
 # SellerStreetName=None, SellerTownName=None, SellerPostCodeIdentifier=None, CountryCode=None, CountryName=None, SellerPostOfficeBoxIdentifier=None
-sellerPostalAddress = SellerPostalAddressDetailsType( "Puukatu 2 F", "HELSINKI", 00112, "FI", "Suomi" )
+sellerPostalAddress = SellerPostalAddressDetailsType( _sellerAddress, _sellerTown, _sellerPostCode, _sellerCountryCode, _sellerCountryName )
 
+sellerOrganisationNames = {}
 # Seller Organization Name
 # LanguageCode=None, SellerOrganisationName=None
-sellerOrganisationNamesFi = SellerOrganisationNamesType( "FI" )
-sellerOrganisationNamesFi.add_SellerOrganisationName( "Pullis Musiken Oy" )
-sellerOrganisationNamesSv = SellerOrganisationNamesType( "SV" )
-sellerOrganisationNamesSv.add_SellerOrganisationName( "Pullis Musiken Ab" )
-sellerOrganisationNamesEn = SellerOrganisationNamesType( "EN" )
-sellerOrganisationNamesEn.add_SellerOrganisationName( "Pullis Musiken Ltd" )
+for (_langCode, _orgName) in _sellerOrganisationName.items():
+	sellerOrganisationNames[_langCode] = SellerOrganisationNamesType( _langCode )
+	sellerOrganisationNames[_langCode].add_SellerOrganisationName( _orgName )
 
+sellerAccountDetails = []
 # SellerAccountID=None, SellerBic=None, NewSellerAccountID=None, NewSellerBic=None
-sellerAccountDetailsOKOYFIHH = SellerAccountDetailsType( SellerAccountIDType( "IBAN", "FI2757800750155448" ), SellerBicType( "BIC", "OKOYFIHH" ) )
-sellerAccountDetailsNDEAFIHH = SellerAccountDetailsType( SellerAccountIDType( "IBAN", "FI2721221222212227" ), SellerBicType( "BIC", "NDEAFIHH" ) )
-sellerAccountDetailsNDEAFIHH = SellerAccountDetailsType( SellerAccountIDType( "IBAN", "FI2781232323312334" ), SellerBicType( "BIC", "PSPBFIHH" ) )
+for _account in _sellerAccounts:
+	sellerAccountDetails.append( SellerAccountDetailsType( SellerAccountIDType( 'IBAN', _account['IBAN'] ), SellerBicType( 'BIC', _account['BIC'] ) ) )
 
 # Sender Information
 # Version=None, MessageDetails=None, SellerPartyDetails=None, SellerOrganisationUnitNumber=None, InvoiceSenderInformationDetails=None,
 # SellerAccountDetails=None, SellerInvoiceDetails=None, ProposedDueDateAccepted=None, ProposedInvoicePeriodAccepted=None
-senderInfo = FinvoiceSenderInfo( "2.0" )
-
-_messageId = "001"
+senderInfo = FinvoiceSenderInfo( '2.0' )
 
 # Message Details
 # MessageTypeCode=None, MessageTypeText=None, MessageActionCode=None, MessageActionCodeIdentifier=None, MessageDate=None, SenderInfoIdentifier=None
-senderInfo.set_MessageDetails( MessageDetailsType( "SENDERINFO", "Sender information", "ADD", "00", nowDate, _messageId ) )
+senderInfo.set_MessageDetails( MessageDetailsType( 'SENDERINFO', 'INVOICER NOTIFICATION', _messageActionCode, None, nowDate, _messageId ) )
 
 # Seller Party Details
 # SellerPartyIdentifier=None, SellerOrganisationNames=None, SellerOrganisationBankName=None, SellerPostalAddressDetails=None, IndustryCode=None
-sellerPartyDetails = SellerPartyDetailsType( "0199920-7", None, None, sellerPostalAddress, 62020 )
-sellerPartyDetails.add_SellerOrganisationNames( sellerOrganisationNamesFi )
-sellerPartyDetails.add_SellerOrganisationNames( sellerOrganisationNamesSv )
-sellerPartyDetails.add_SellerOrganisationNames( sellerOrganisationNamesEn )
+sellerPartyDetails = SellerPartyDetailsType( _sellerYTunnus, None, None, sellerPostalAddress, _sellerIndustryCode )
+
+for (_langCode, _orgName) in sellerOrganisationNames.items():
+	sellerPartyDetails.add_SellerOrganisationNames( _orgName )
 
 senderInfo.set_SellerPartyDetails( sellerPartyDetails )
 
 # SellerWebaddressNameText=None, SellerWebaddressText=None, InvoiceSenderAddress=None, InvoiceSenderIntermediatorAddress=None, NewInvoiceSenderAddress=None, NewInvoiceSenderIntermediatorAddress=None
-senderInfo.set_InvoiceSenderInformationDetails( InvoiceSenderInformationDetailsType( "Pullin Musiikki Oy", "https://www.pullinmusiikki.fi/", "00371999207", "OKOYFIHH" ) )
+senderInfo.set_InvoiceSenderInformationDetails( InvoiceSenderInformationDetailsType( _sellerWebAddressNameText, _sellerWebAddress, _sellerInvoiceAddress, _sellerInvoiceIntermediatorAddress ) )
 
-senderInfo.add_SellerAccountDetails( sellerAccountDetailsOKOYFIHH )
-senderInfo.add_SellerAccountDetails( sellerAccountDetailsNDEAFIHH )
-
-# SellerInvoiceTypeText=None, SellerInvoiceIdentifierText=None
-sellerInvoiceTypeDetailsFi = SellerInvoiceTypeDetailsType( SellerInvoiceTypeTextType( "FI", "Kirjanpito palvelu" ) )
-sellerInvoiceTypeDetailsSv = SellerInvoiceTypeDetailsType( SellerInvoiceTypeTextType( "SV", "Bokföring tjänster" ) )
-sellerInvoiceTypeDetailsEn = SellerInvoiceTypeDetailsType( SellerInvoiceTypeTextType( "EN", "Bookkeeping service" ) )
-
-#LanguageCode=None, SellerInvoiceIdentifierType=None, SellerInvoiceIdentifierMinLength=1, SellerInvoiceIdentifierHyphens=False, SellerInvoiceIdentifierSpaces=False, SellerInvoiceIdentifierMaxLength=35, valueOf_=None, extensiontype_=None
-sellerInvoiceTypeDetailsFi.add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( "FI", "02", None, None, None, None, "Viitenumero" ) )
-sellerInvoiceTypeDetailsFi.add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( "FI", "09", 10, True, False, 10, "Asiakasnumero" ) )
-
-sellerInvoiceTypeDetailsSv.add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( "SV", "02", None, None, None, None, "Referensnummer" ) )
-sellerInvoiceTypeDetailsSv.add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( "SV", "04", 10, True, False, 10, "Kundnummer" ) )
-
-sellerInvoiceTypeDetailsEn.add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( "EN", "02", None, None, None, None, "Reference number" ) )
-sellerInvoiceTypeDetailsEn.add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( "EN", "04", 10, True, False, 10, "Customer number" ) )
+for _account in sellerAccountDetails:
+	senderInfo.add_SellerAccountDetails( _account )
 
 # SellerDirectDebitIdentifier=None, PaymentInstructionIdentifier=None, SellerInstructionFreeText=None, SellerInvoiceTypeDetails=None, SellerServiceCode=None
-sellerInvoiceDetails = SellerInvoiceDetailsType( None, "Bookkeeping service" )
-sellerInvoiceDetails.add_SellerInvoiceTypeDetails( sellerInvoiceTypeDetailsFi )
-sellerInvoiceDetails.add_SellerInvoiceTypeDetails( sellerInvoiceTypeDetailsSv )
-sellerInvoiceDetails.add_SellerInvoiceTypeDetails( sellerInvoiceTypeDetailsEn )
+sellerInvoiceDetails = SellerInvoiceDetailsType( None, _paymentInstructionId )
 
-sellerInvoiceDetails.set_SellerServiceCode( "00" )
+sellerInvoiceTypeDetails = {}
+for (_langCode, _type) in _sellerInvoiceTypeDetails.items():
+	# SellerInvoiceTypeText=None, SellerInvoiceIdentifierText=None
+	sellerInvoiceTypeDetails[_langCode] = SellerInvoiceTypeDetailsType( SellerInvoiceTypeTextType( _langCode, _type['text'] ) )
+
+	for _validation in _type['validation']:
+		#LanguageCode=None, SellerInvoiceIdentifierType=None, SellerInvoiceIdentifierMinLength=1, SellerInvoiceIdentifierHyphens=False, SellerInvoiceIdentifierSpaces=False, SellerInvoiceIdentifierMaxLength=35, valueOf_=None, extensiontype_=None
+		sellerInvoiceTypeDetails[_langCode].add_SellerInvoiceIdentifierText( SellerInvoiceIdentifierTextType( _langCode, _validation['type'], _validation['min'], _validation['hyphens'], _validation['spaces'], _validation['max'], _validation['text'] ) )
+
+	sellerInvoiceDetails.add_SellerInvoiceTypeDetails( sellerInvoiceTypeDetails[_langCode] )
+
+# = e-invoicer
+sellerInvoiceDetails.set_SellerServiceCode( '00' )
 
 senderInfo.set_SellerInvoiceDetails( sellerInvoiceDetails )
 
-senderInfo.set_ProposedDueDateAccepted( "NO" )
-senderInfo.set_ProposedInvoicePeriodAccepted( "YES" )
+senderInfo.set_ProposedDueDateAccepted( _proposedDueDate )
+senderInfo.set_ProposedInvoicePeriodAccepted( _proposedPaymentPeriod )
 
-# Header=None, Body=None
-envelope = Envelope(  )
-
-# mustUnderstand=None, version=None, From=None, To=None, CPAId=None, ConversationId=None, Service=None, Action=None, MessageData=None
-messageHeader = MessageHeader( 1, "2.0" )
-
-header = Header( )
-header.add_anytypeobjs_( messageHeader )
-
-# PartyId=None, Role=None
-msgFrom = From( None, "Sender" )
-msgFrom.add_PartyId( PartyId( None, "00371999207" ) )
-
-msgFromI = From( None, "Intermediator" )
-msgFromI.add_PartyId( PartyId( None, "OKOYFIHH" ) )
-
-messageHeader.add_anytypeobjs_( msgFrom )
-messageHeader.add_anytypeobjs_( msgFromI )
-
-msgTo = To( None, "Receiver" )
-msgTo.add_PartyId( PartyId( None, "SENDERINFO" ) )
-
-msgToI = To( None, "Intermediator" )
-msgToI.add_PartyId( PartyId( None, "OKOYFIHH" ) )
-
-messageHeader.add_anytypeobjs_( msgTo )
-messageHeader.add_anytypeobjs_( msgToI )
-
-messageHeader.set_CPAId( "yoursandmycpa" )
-
-messageHeader.set_Service( Service( None, "Routing" ) )
-messageHeader.set_Action( "ProcessInvoice" )
+_recepients = [
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'OKOYFIHH',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'NDEAFIHH',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'DABAFIHH',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'HELSFIHH',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'TAPIFI22',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'AABAFI22',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'HANDFIHH',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'SBANFIHH',
+	},
+	{
+		'Receiver': 'SENDERINFO',
+		'Intermediator': 'DNBAFIHX',
+	}
+]
 
 _now = datetime.datetime.now( reference.LocalTimezone() )
 _nowS = datetime.datetime( _now.year, _now.month, _now.day, _now.hour, _now.minute, _now.second, 0, _now.tzinfo )
-msgData = MessageData( _messageId + '/1', _nowS )
 
-messageHeader.set_MessageData( msgData )
+for (i, _recepient) in enumerate(_recepients):
+	envelope = Envelope(  )
 
-# messageHeader.export( sys.stdout, 0, namespace_='eb:', name_='MessageHeader', namespacedef_='xmlns:eb="http://www.oasis-open.org/committees/ebxml-msg/schema/msg-header-2_0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.oasis-open.org/committees/ebxml-msg/schema/msg-header-2_0.xsd msg-header-2_0.xsd"', pretty_print=True )
+	# mustUnderstand=None, version=None, From=None, To=None, CPAId=None, ConversationId=None, Service=None, Action=None, MessageData=None
+	messageHeader = MessageHeader( 1, "2.0" )
 
-envelope.set_Header( header )
+	# Header=None, Body=None
+	header = Header( )
+	header.add_anytypeobjs_( messageHeader )
 
-manifest = Manifest( "2.0", "Manifest" )
-reference = Reference( None, _messageId, None, "FinvoiceSenderInfo" )
-manifest.add_Reference( reference )
-reference.add_Schema( Schema( "2.0", "http://www.pankkiyhdistys.fi/verkkolasku/finvoice/FinvoiceSenderInfo.xsd" ) )
+	# PartyId=None, Role=None
+	msgFrom = From( None, "Sender" )
+	msgFrom.add_PartyId( PartyId( None, _sellerInvoiceAddress ) )
 
-body = Body( )
-body.add_anytypeobjs_( manifest )
+	msgFromI = From( None, "Intermediator" )
+	msgFromI.add_PartyId( PartyId( None, _sellerInvoiceIntermediatorAddress ) )
 
-envelope.set_Body( body )
+	messageHeader.add_anytypeobjs_( msgFrom )
+	messageHeader.add_anytypeobjs_( msgFromI )
 
-envelope.export( sys.stdout, 0, pretty_print=True )
+	msgTo = To( None, "Receiver" )
+	msgTo.add_PartyId( PartyId( None, _recepient['Receiver'] ) )
 
-encodingHeader = '<?xml version="1.0" encoding="' + ExternalEncoding + '" ?>\n'
-sys.stdout.write( encodingHeader )
-sys.stdout.write( '<?xml-stylesheet type="text/xsl" href="FinvoiceSenderInfo.xsl"?>\n' )
-senderInfo.export( sys.stdout, 0, name_='FinvoiceSenderInfo', namespacedef_='xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="FinvoiceSenderInfo.xsd"', pretty_print=True )
+	msgToI = To( None, "Intermediator" )
+	msgToI.add_PartyId( PartyId( None, _recepient['Intermediator'] ) )
+
+	messageHeader.add_anytypeobjs_( msgTo )
+	messageHeader.add_anytypeobjs_( msgToI )
+
+	messageHeader.set_CPAId( "yoursandmycpa" )
+
+	messageHeader.set_Service( Service( None, "Routing" ) )
+	messageHeader.set_Action( "ProcessInvoice" )
+
+	msgData = MessageData( '{0}/{1}'.format(_messageId, i), _nowS )
+
+	messageHeader.set_MessageData( msgData )
+
+	envelope.set_Header( header )
+
+	manifest = Manifest( "2.0", "Manifest" )
+	reference = Reference( None, _messageId, None, "FinvoiceSenderInfo" )
+	manifest.add_Reference( reference )
+	reference.add_Schema( Schema( "2.0", "http://www.pankkiyhdistys.fi/verkkolasku/finvoice/FinvoiceSenderInfo.xsd" ) )
+
+	body = Body( )
+	body.add_anytypeobjs_( manifest )
+
+	envelope.set_Body( body )
+
+	envelope.export( sys.stdout, 0, pretty_print=True )
+
+	encodingHeader = '<?xml version="1.0" encoding="' + ExternalEncoding + '" ?>\n'
+	sys.stdout.write( encodingHeader )
+	sys.stdout.write( '<?xml-stylesheet type="text/xsl" href="FinvoiceSenderInfo.xsl"?>\n' )
+	senderInfo.export( sys.stdout, 0, name_='FinvoiceSenderInfo', namespacedef_='xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="FinvoiceSenderInfo.xsd"', pretty_print=True )
--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
 		author='Mohanjith Sudirikku Hannadige',
 		author_email='moha@mohanjith.net',
 		url='http://www.codemaster.fi/python/finvoice/',
-		packages=[ 'finvoice', 'finvoice.ack', 'finvoice.attachment', 'finvoice.receiver', 'finvoice.sender' ]
+		packages=[ 'finvoice', 'finvoice.ack', 'finvoice.attachment', 'finvoice.receiver', 'finvoice.sender', 'finvoice.soap' ]
 )
